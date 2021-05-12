21633f2537cdeb2b17d7742b5abde1804a5230f4
volodymyr1213 <vova.khomenko.1990@gmail.com>
2021-05-11 19:00:03 -0500

    added a nagios builder and sh nagios

---
+++ nagiosxi/nagios.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install curl -y
+sudo yum install wget -y
+sudo yum install unzip -y
+sudo curl https://assets.nagios.com/downloads/nagiosxi/install.sh | sh
+sudo cd /tmp
+sudo wget https://assets.nagios.com/downloads/nagiosxi/xi-latest.tar.gz
+sudo tar xzf xi-latest.tar.gz
+sudo cd nagiosxi
+sudo ./fullinstall
